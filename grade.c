7f9283f1b4bf609cba49180b963c4597e90cb9f8
Gyanendra Sinha <70687470+gyanendrasinha8301@users.noreply.github.com>
2020-09-06 19:31:35 +0530

    Create grade.c

---
+++ grade.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{   int marks;
+    printf("Enter Marks: ");
+    scanf("%d",&marks);
+    printf("\n");
+    if (marks<=100 && marks>=85)
+        {printf("Grade A");}
+    else if (marks<=84 && marks>=70)
+        {printf("Grade B");}
+    else if (marks<=69 && marks>=55)
+        {printf("Grade C");}
+    else if (marks<=54 && marks>=40)
+        {printf("Grade D");}
+    else if (marks<40)
+        {printf("Grade F");}
+    return 0;
+}
